30f0fd80132a9bc8c8077b5ee729d3c2e11d9a85
Jonathan Rudd <jonathanrudd3@gmail.com>
2022-07-06 21:43:02 +0900

    refactoring routes

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,50 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    restaurant = Restaurant.find(params[:id])
+    restaurant.update(restaurant_params)
+    redirect_to restaurant_path(restaurant)
+  end
+
+  def destroy
+    # fetch restaurant to destroy from DB
+    restaurant = Restaurant.find(params[:id])
+
+    # destroy record
+    restaurant.destroy
+
+    # redirect to index
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :stars)
+  end
+end
