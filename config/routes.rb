4c81694399a1334f441c40ba088982e826cc0873
Jonathan Rudd <jonathanrudd3@gmail.com>
2022-07-07 20:02:15 +0900

    created nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,30 @@
 Rails.application.routes.draw do
 
-  resources :reviews
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:create]
+  end
+
+
+
+
+
+
+
+
+
+  #resources :reviews
 
   # A visitor can add a new review to a restaurant
   # get    "restaurants/:id/reviews/new", to: "restarant#review#new"
   # post   "restaurants/:id/reviews", to: ""
 
 
-  get    "restaurants",          to: "restaurants#index"
+  #get    "restaurants",          to: "restaurants#index"
 
-  get    "restaurants/new",      to: "restaurants#new",  as: :new_restaurant
-  post   "restaurants",          to: "restaurants#create"
+  #get    "restaurants/new",      to: "restaurants#new",  as: :new_restaurant
+  #post   "restaurants",          to: "restaurants#create"
 
   # NB: The `show` route needs to be *after* `new` route.
-  get    "restaurants/:id",      to: "restaurants#show", as: :restaurant
+  # get    "restaurants/:id",      to: "restaurants#show", as: :restaurant
 
 end
